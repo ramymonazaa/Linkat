f24564e55013676b3bb08481011529595ba7a861
ramymonazaa <romeo33667@gmail.com>
2022-10-02 11:38:31 +0200

    Remove a hardcoded Django secret key

--- linky/settings.py
+++ linky/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,3 +124,4 @@ STATICFILES_DIRS=[
     ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
