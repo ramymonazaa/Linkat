59052e37ca5cc2e63c1480acd15575ea5e9e3ab9
ramymonazaa <romeo33667@gmail.com>
2022-10-02 10:51:22 +0200

    Automatic configuration with django-heroku

--- linky/settings.py
+++ linky/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
+django_heroku.settings(locals())
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+PORT = os.getenv("PORT", default="5000")
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
