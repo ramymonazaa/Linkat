7cf7aa3d0b038d8dd62369dcd1488c50bc9afa5c
ramymonazaa <romeo33667@gmail.com>
2022-10-02 12:37:00 +0200

    'ss'

--- linky/settings.py
+++ linky/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4$+h)9!yg&f4(-lijmxxus1pi_@$xo5q7qpd^cp%r2&v^vr@s$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [   ]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'linky/static')
     ]
